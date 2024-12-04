66397bd58bd374fb0d71a1e351caeecc4c0c07ed
Linay90 <linaybharambe1108@gmail.com>
2024-12-04 12:15:39 +0530

    started working on frontend

---
+++ React+Go/Frontend/gomovies/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() { 
+  return (
+    <div className="container">
+      <h1 className="row">
+        Go watch a Movie!
+      </h1>
+      <hr className="mb-3"></hr>
+    </div>
+    
+
+    <div className="row">
+      <div className="col-md-2">
+
+    </div>
+  );
+}
+
+export default App;
